98b6106be196f9cfe07abfab9859a6328b337334
Himani Joshi <himanijoshi28@gmail.com>
2024-01-27 10:47:53 +0530

    fetchevery10 sec

--- week6/simpletodoapp/src/App.jsx
+++ week6/simpletodoapp/src/App.jsx
@@ -1,20 +1,56 @@
-import React from "react";
-import CardWrapper from "./components/CardWrapper";
-import TextComponent from "./components/TextComponent";
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [todos, setTodo] = useState([
+//     {
+//       title: "title",
+//     },
+//   ]);
+//   return (
+//     <div>
+//       {todos.map((todo) => {
+//         <p>{todo.title}</p>;
+//         console.log(todo.title);
+//       })}
+//     </div>
+//   );
+// };
+
+// export default App;
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch(`https://sum-server.100xdevs.com/todos`).then(async function (
+        response
+      ) {
+        const json = await response.json();
+        setTodos(json.todos);
+        console.log(todos);
+      });
+    }, 10000);
+  }, []);
+  return (
+    <>
+      hello
+      {todos.map((todo) => (
+        <Todo title={todo.title} />
+      ))}
+    </>
+  );
+};
+const Todo = ({ title }) => {
   return (
     <div>
-      <CardWrapper innercomponent={<TextComponent />} />
+      <h1>{title}</h1>
+      {/* <h5>{description}</h5> */}
     </div>
   );
 };
-// const CardWrapper = ({ innercomponent }) => {
-//   return <div style={{ border: "2px solid black " }}>{innercomponent}</div>;
-// };
-
-// const TextComponent = () => {
-//   return <div>Hey this is text compo</div>;
-// };
 
 export default App;
