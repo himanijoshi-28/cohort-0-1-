3ff7d9a4dcba440f44841d5042b341dcec29edb2
Himani Joshi <himanijoshi28@gmail.com>
2024-01-30 10:56:44 +0530

    contextapi

--- week7/week7-1/src/App.jsx
+++ week7/week7-1/src/App.jsx
@@ -1,41 +1,45 @@
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import Landing from "./components/Landing";
-import Dashboard from "./components/Dashboard";
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Appbar />
-        <Routes>
-          <Route path="/" element={<Landing />}></Route>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
-function Appbar() {
-  const navigate = useNavigate();
+/* eslint-disable react/prop-types */
+import { useContext, useState } from "react";
+import { countContext } from "./context.js";
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <div>
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Landing
-        </button>
-        <button
-          onClick={() => {
-            navigate("/dashboard");
-          }}
-        >
-          Dashboard
-        </button>
-      </div>
+      <countContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </countContext.Provider>
+    </div>
+  );
+};
+function Count({ setCount }) {
+  const count = useContext(countContext);
+  return (
+    <div>
+      {count}
+      <Buttons count={count} setCount={setCount} />;
     </div>
   );
 }
+function Buttons({ setCount }) {
+  let count = useContext(countContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count++);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count--);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
 export default App;
