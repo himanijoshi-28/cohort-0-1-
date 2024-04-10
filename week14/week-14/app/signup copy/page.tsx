ba460b0c78f1aee3b8508b791c1400fe1c10535a
Himani Joshi <himanijoshi28@gmail.com>
2024-04-10 10:17:36 +0530

    last commit

--- week14/week-14/app/signup copy/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function signup() {
-  return (
-  <p>hey</p>
-  );
-}
--- week14/week-14/app/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function signup() {
-  return (
-   <div className="">
-    signup
-   </div>
-  );
-}
